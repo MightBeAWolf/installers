4ef0cd3b374b37faedcf4772d1ad47b18582c270
Thomas G Wolf <5043711+MightBeAWolf@users.noreply.github.com>
2023-11-18 10:38:42 -0700

    rust-cli tools are now filtered by those not yet installed

--- 01-rust-cli-tools.sh
+++ 01-rust-cli-tools.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-
-cli_utilities=(
- "fd-find"
- "ripgrep"
- "du-dust"
- "helix-term"
- "eza"
- "bat"
- "procs"
- "bandwhich"
- "tealdeer"
- "git-delta"
- "sd"
- "bottom"
-)
-
-# Make sure that the cargo binary is in the path
-export PATH="${PATH}:/home/${SUDO_USER:-${USER}}/.cargo/bin"
-if which cargo >/dev/null 2>/dev/null; then 
-  for util in "${cli_utilities[@]}"; do
-    echo "Installing $util"
-    if cargo install "${util}"; then 
-      echo "Installed ${util}"
-    else
-      echo "Failed to install ${util}"
-      exit 1
-    fi
-  done
-else
+if ! which cargo >/dev/null 2>/dev/null; then 
   echo "Cargo is not installed."
   echo "Please install Cargo before running this script"
   exit 1  
 fi
+
+cli_utilities=(
+ "fd-find:fd"
+ "ripgrep:rg"
+ "du-dust"
+ "eza"
+ "bat"
+ "procs"
+ "bandwhich"
+ "tealdeer:tldr"
+ "git-delta:delta"
+ "sd"
+ "bottom:btm"
+)
+
+# Make sure that the cargo binary is in the path
+export PATH="${PATH}:/home/${SUDO_USER:-${USER}}/.cargo/bin"
+for util in "${cli_utilities[@]}"; do
+  package="${util%%:*}"
+  binary="${util##*:}"
+  if ! which "${binary:?}" >/dev/null 2>/dev/null; then
+    echo "Installing ${package:?}"
+    if cargo install "${package:?}"; then 
+      echo "Installed ${package:?}"
+    else
+      echo "Failed to install ${package:?}"
+      exit 1
+    fi
+  fi
+done
