0f165e4a847aad5dcb2f0bea11317478a4ada5c3
Thomas G Wolf <gwolf@silicontechnologiesinc.com>
2023-12-20 11:43:04 -0700

    Helix will now checkout the latest released version

--- 01-helix.sh
+++ 01-helix.sh
@@ -11,6 +11,7 @@ trap cleanup EXIT ERR INT TERM
 
 # Create a temporary directory
 TMP_DIR=$(mktemp -d -t helix-install-XXXXXXXXXX)
+cd "${TMP_DIR:?}"
 
 # If the user's .cargo bin directory exists,
 # but doesn't exist in the path, update the path.
@@ -29,10 +30,20 @@ for dep in "${dependencies[@]}"; do
 done
 
 # Clone the Helix repository if it does not exist
-git clone https://github.com/helix-editor/helix "$TMP_DIR/helix"
+git clone https://github.com/helix-editor/helix "./helix"
+cd "./helix"
+
+# Checkout the latest stable release
+LATEST_RELEASE="$( git ls-remote -q --tags \
+    | awk '{print $2}' \
+    | xargs -I{} git log -1 --format='%at %H' {} \
+    | sort -h \
+    | tail -n 1 \
+    | awk '{print $2}')"
+git checkout "${LATEST_RELEASE:?}"
 
 # Build and install Helix from source
-if ! cargo install --path "$TMP_DIR/helix/helix-term" --locked; then 
+if ! cargo install --path "./helix-term" --locked; then 
     echo "Failed to install helix!"
     exit 1
 fi
@@ -42,7 +53,7 @@ mkdir -p "$HOME/.config/helix"
 if [[ -e "$HOME/.config/helix/runtime" ]]; then 
     rm -rf "$HOME/.config/helix/runtime"
 fi
-mv "$TMP_DIR/helix/runtime" "$HOME/.config/helix"
+mv "./runtime" "$HOME/.config/helix"
 
 # Fetch and compile tree-sitter grammars
 "$HOME/.cargo/bin/hx" --grammar fetch
